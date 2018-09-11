254346ecb8c89cfe661c241399edc4b21ebfbedf
VSurin <V.Surin@evnedev.com>
2018-09-11 15:08:28 +0300

    сделал вывод количества постов API

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,18 @@ Route::get('/', function () {
 Route::resource('category','CategoryController');
 Route::resource('posts','PostController');
 
-Route::get('api/posts/', function () {
-	$posts = App\Post::all();
+Route::get('api/posts/{id}', function ($id = 1) {
+    $offest = ($id - 1) * 2;
+
+	$posts = DB::table('post')
+        ->offset($offest)
+        ->limit(2)
+        ->get();
+
 	return response()->json(['posts' => $posts]); 
+});
+Route::get('api/count/posts/', function () {
+    return response()->json(['count' => App\Post::count()]);
 });
 Route::get('api/posts/{post}', function (App\Post $post) {
     return response()->json(['posts' => $post]); 
