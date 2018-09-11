cf051d1e0f6f0a157b38be9f8970f3db671a5b17
VSurin <V.Surin@evnedev.com>
2018-09-11 15:59:33 +0300

    Добавил ckeditor и вывел его в редактирование и создание поста

--- resources/views/post/form.blade.php
+++ resources/views/post/form.blade.php
@@ -2,10 +2,10 @@
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
             <strong>Title:</strong>
-            {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!}            
+            {!! Form::text('title', null, array('placeholder' => 'Title','class' => 'form-control')) !!}
 
             <strong>Content:</strong>
-            {!! Form::text('content', null, array('placeholder' => 'Content','class' => 'form-control')) !!}                  
+            {{ Form::textarea('content', null, array('id'=>'editor')) }}
 
             <strong>Category:</strong>
             {!! Form::select('id_category', $categories, null, ['class' => 'form-control']) !!}            
@@ -19,3 +19,8 @@
             <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 </div>
+
+<script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
+<script type="text/javascript">
+    CKEDITOR.replace( 'editor' );
+</script>
